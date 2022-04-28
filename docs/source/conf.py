02439c067d5f0b3dec3cce41acd9669154957e2d
hivanov-nrel <53838851+hivanov-nrel@users.noreply.github.com>
2022-04-28 12:34:52 -0600

    more pip bug fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.githubpages",
-    "nbsphinx",
+    #"nbsphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
